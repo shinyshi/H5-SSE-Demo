cbf10cf4764710ae32cc370fa9f36cd320286b44
shirx <dcy2804@dingtalk.com>
2019-07-22 00:10:01 +0800

    first upload

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public void Talk()
+        {
+            Response.ContentType = "text/event-stream";
+            Response.ContentEncoding = Encoding.UTF8;
+            Response.Headers.Add("Cache-Control", "no-cache");
+            Response.Headers.Add("Connection", "keep-alive");
+            Response.Write("data: " + DateTime.Now + "\n\nretry:" + 10000 + "\n\n");
+            Response.Flush();
+                
+            
+        }
+
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication5/Controllers/TalkController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace WebApplication5.Controllers
+{
+    public class TalkController : ApiController
+    {
+        ///// <summary>
+        ///// 推送消息
+        ///// </summary>
+        ///// <returns></returns>
+        [HttpGet]
+        public HttpResponseMessage SentNews()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            //response.Headers.Add("Access-Control-Allow-Origin", "*");//如需要跨域可配置
+            string data_str = "good day";//当然可以是json字符串格式
+            string even = "", data = "";
+            if (!string.IsNullOrWhiteSpace(data_str))
+            {
+                even = "event:sentMessage\n";
+                data = "data:" + data_str + "\n\n";
+            }
+            string retry = "retry:" + 1000 + "\n";//连接断开后重连时间（毫秒），其实可以理解为轮询时间 2333...
+            byte[] array = Encoding.UTF8.GetBytes(even + data + retry);
+            Stream stream_result = new MemoryStream(array);
+            response.Content = new StreamContent(stream_result);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/event-stream");//此处一定要配置
+            response.Headers.Add("Connection", "keep-alive");
+            response.Headers.CacheControl = new CacheControlHeaderValue();
+            response.Headers.CacheControl.NoCache = true;
+            
+            return response;
+        }
+    }
+}
